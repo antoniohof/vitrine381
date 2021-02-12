a5292ecbf7bf602daee059c7fb460fc821396f7e
Antonio Hofmeister <hof.antonio@gmail.com>
2021-02-12 11:32:29 +0100

    first commit - nuxt content boilerplate

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container fluid>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  mounted () {
+    console.log('mounted vitrine app')
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    VITRINE
+  </div>
+</template>
+
+<script>
+export default {
+  head () {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }]
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+  },
+  components: {
+  },
+  async asyncData({ $content }) {
+    /*
+    const posts = await $content("blog").fetch()
+
+    return {
+      posts
+    }
+    */
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
