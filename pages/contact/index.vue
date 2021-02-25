d521f059aa95ed024dafa69ab711e93801b4f73e
Antonio Hofmeister <hof.antonio@gmail.com>
2021-02-25 15:45:25 +0100

    fix e-mail

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -7,7 +7,7 @@
         </h1>
         <p>
        <!-- Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.-->
-       lstoever@hfk-bremen.de
+       hallo@vitrine381.space
         </p>
         <p>
        Gröpelinger Heerstraße 381 Bremen
