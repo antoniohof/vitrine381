ea17f77e148ce3e0b14a5c1aa702c77e7a474b61
Antonio Hofmeister <hof.antonio@gmail.com>
2021-02-25 15:25:03 +0100

    index on google

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'creative space' }
+      { hid: 'description', name: 'description', content: 'creative space' },
+      { name="google-site-verification", content="4ynK4ZHvcYfOkAJNCMiLlV5RgJNnzCgZWA8Mxjh5zys"}
     ],
     link: [
       {
